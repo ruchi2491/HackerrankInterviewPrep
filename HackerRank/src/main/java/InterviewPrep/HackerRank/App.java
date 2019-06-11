f8e5e4e047db792c6be1f1f77275ec44a02bb16f
ruchi2491 <ruchi.more01@gmail.com>
2019-06-11 17:06:37 +0530

    first commit

---
+++ HackerRank/src/main/java/InterviewPrep/HackerRank/App.java
@@ -0,0 +1,13 @@
+package InterviewPrep.HackerRank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
